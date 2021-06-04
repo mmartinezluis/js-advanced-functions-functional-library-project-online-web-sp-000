7ec187dbfcaca4c83df357510b8886f686baaa56
Luis Martinez <luis_mmartinez@live.com>
2021-06-03 21:19:59 -0400

    commit tree d2fe52d918b86050a430add9345756b2a1aa239c

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, alert) { 
+      if (collection.length === undefined) {
+        let values = Object.values(collection)
+        for ( let i = 0; i < values.length; i++) {
+          alert(values[i]);
+        }
+      } else {
+          for (let i = 0 ; i < collection.length; i++) {
+            alert(collection[i]);
+          }
+      }
+      return collection;
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      let  array = []
 
+      if (collection.length === undefined){
+        let values = Object.values(collection)
+        for ( let i = 0; i < values.length; i++) {
+          array.push(callback(values[i]));
+       }
+      } else {
+          for ( let i = 0; i < collection.length; i++) {
+            array.push(callback(collection[i]));
+          }
+      }
+      return array;
     },
 
     reduce: function() {
